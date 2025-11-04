4e13de4510e66fe650ea00d40442a0d65bb3d76b
Icebound777 <frozenblue@gmx.de>
2025-11-04 21:10:41 +0100

    feat: add vanilla game struct

--- src/seed_generation/mod.rs
+++ src/seed_generation/mod.rs
@@ -1,3 +1,5 @@
-pub mod seed_settings;
-pub mod seed_gen_main;
+pub mod randomization_datastructures;
+pub mod randomize_game;
 pub mod rom_patching;
+pub mod seed_gen_main;
+pub mod seed_settings;
---
+++ src/seed_generation/randomization_datastructures.rs
@@ -0,0 +1,114 @@
+use crate::seed_generation::seed_settings::{BossGarageRequirements, FinalOxideUnlock, RelicTime};
+
+pub struct RandomizedGame {
+    pub warppad_links: Vec<(LevelID, LevelID)>,
+    pub warppad_unlocks: Vec<(LevelID, UnlockStage, UnlockRequirement)>,
+    pub race_rewards: Vec<(LevelID, RaceType, RaceReward)>,
+    pub settings: Vec<(SettingID, SettingValue)>,
+}
+
+pub enum LevelID {
+    DingoCanyon = 0,
+    DragonMines = 1,
+    BlizzardBluff = 2,
+    CrashCove = 3,
+    TigerTemple = 4,
+    PapusPyramid = 5,
+    RoosTubes = 6,
+    HotAirSkyway = 7,
+    SewerSpeedway = 8,
+    MysteryCaves = 9,
+    CortexCastle = 10,
+    NGinLabs = 11,
+    PolarPass = 12,
+    OxideStation = 13,
+    CocoPark = 14,
+    TinyArena = 15,
+    SlideColiseum = 16,
+    TurboTrack = 17,
+    NitroCourt = 18,
+    RampageRuins = 19,
+    SkullRock = 21,
+    RockyRoad = 23,
+    CupRed = 100,
+    CupGreen = 101,
+    CupBlue = 102,
+    CupYellow = 103,
+    CupPurple = 104,
+}
+
+pub enum UnlockStage {
+    One,
+    Two,
+}
+
+pub struct UnlockRequirement {
+    pub item_type: RequiredItem,
+    pub count: u8,
+}
+
+pub enum RequiredItem {
+    Trophy = 98,
+    RedCtrToken = 381,
+    GreenCtrToken = 637,
+    BlueCtrToken = 893,
+    YellowCtrToken = 1149,
+    PurpleCtrToken = 1405,
+    AnyCtrToken = 1917,
+    SapphireRelic = 97,
+    GoldRelic = 353,
+    PlatinumRelic = 609,
+    AnyRelic = 1889,
+    Key = 99,
+    RedGem = 95,
+    GreenGem = 351,
+    BlueGem = 607,
+    YellowGem = 863,
+    PurpleGem = 1119,
+    AnyGem = 1887,
+}
+
+pub enum RaceType {
+    TrophyRace,
+    CtrOrCrystalChallenge,
+    RelicRaceSapphire,
+    RelicRaceGold,
+    RelicRacePlatinum,
+    BossRace,
+    GemCup,
+}
+
+pub enum RaceReward {
+    Trophy = 98,
+    RedCtrToken = 381,
+    GreenCtrToken = 637,
+    BlueCtrToken = 893,
+    YellowCtrToken = 1149,
+    PurpleCtrToken = 1405,
+    SapphireRelic = 97,
+    GoldRelic = 353,
+    PlatinumRelic = 609,
+    Key = 99,
+    RedGem = 95,
+    GreenGem = 351,
+    BlueGem = 607,
+    YellowGem = 863,
+    PurpleGem = 1119,
+}
+
+pub enum SettingID {
+    RelicDifficulty = 0,
+    RelicNeedsPerfect = 1,
+    BossGarageRequirements = 2,
+    QolSkipMaskhints = 3,
+    QolSkipPodium = 4,
+    QolSkipMaskcongrats = 5,
+    OxideRequiredRelics = 6,
+}
+
+pub enum SettingValue {
+    Boolean(bool),
+    RelicDifficulty(RelicTime),
+    BossGarageRequirements(BossGarageRequirements),
+    OxideRequiredRelics(FinalOxideUnlock),
+}
