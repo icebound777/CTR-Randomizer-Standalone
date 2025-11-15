56fbda54ebb35a682e3dc5db3c6574ac55f48ea9
Icebound777 <frozenblue@gmx.de>
2025-11-15 23:19:35 +0100

    feat: embed base_patch in executable

--- src/seed_generation/rom_patching/bsdiff_patching.rs
+++ src/seed_generation/rom_patching/bsdiff_patching.rs
@@ -9,11 +9,10 @@ pub fn apply_patchfile(old_rom_path: &str, seed: u32) -> Result<PathBuf, Box<dyn
     patchfile_path.push("base_patch.bsdiff4");
 
     let old_rom = std::fs::read(old_rom_path)?;
-    //let patch = std::fs::read(patchfile_path)?;
-    let patch = std::fs::read("/home/icebound/work/Git/CTRRandomizer-Standalone/res/base_patch.bsdiff4")?;
+    let patch = include_bytes!("../../../res/base_patch.bsdiff4");
 
     let mut new_rom = Vec::new();
-    let patcher = Bspatch::new(&patch)?;
+    let patcher = Bspatch::new(patch)?;
     patcher.apply(&old_rom, io::Cursor::new(&mut new_rom))?;
 
     let mut new_rom_path = PathBuf::from(old_rom_path);
