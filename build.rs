cad54064e46d79b06291587c76b0132d2b2d7a78
Icebound777 <frozenblue@gmx.de>
2026-03-27 17:53:10 +0100

    fix: windows icon wrong format

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() {
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
         WindowsResource::new()
             // This path can be absolute, or relative to your crate root.
-            .set_icon("res/CTR_logo.png")
+            .set_icon("res/CTR_logo.ico")
             .compile()
             .expect("Windows build failed");
     }
